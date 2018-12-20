c3740bf41876e44e88176649dff430bfe217854c
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2018-12-20 14:54:17 -0600

    Problem 1 handles number 3

--- src/problem_1/data.py
+++ src/problem_1/data.py
@@ -1,3 +1,6 @@
 
 def get_eligible_numbers(max_number):
+    if max_number == 3:
+        return [3]
+
     return []
--- test/test_problem_1/data_test.py
+++ test/test_problem_1/data_test.py
@@ -7,3 +7,7 @@ class TestData:
     def test_get_eligible_numbers_returns_empty_list_for_less_than_3(self):
         numbers = get_eligible_numbers(2)
         assert numbers == []
+
+    def test_get_eligible_numbers_returns_expected_for_3(self):
+        numbers = get_eligible_numbers(3)
+        assert numbers == [3]
