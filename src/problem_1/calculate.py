4f1cf9249b7924eb0da919c2d7948353bb0f57ba
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2018-12-20 15:43:59 -0600

    Calcluate sum of eligible numbers

--- src/problem_1/calculate.py
+++ src/problem_1/calculate.py
@@ -1,3 +1,5 @@
+from .data import get_eligible_numbers
 
 def get_sum_of_numbers_divisible_by_three_and_five(max_number):
-    pass
+    numbers = get_eligible_numbers(max_number)
+    return sum(numbers)
