3c106d307431d51cc6ae6c591c6591c3b300fb70
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2018-12-25 18:33:46 -0600

    Problem 2 solve script

---
+++ src/problem_2/solve.py
@@ -0,0 +1,6 @@
+from solution.calculate import get_sum_of_even_numbers_in_fibonacci_sequence
+
+value = 4000000
+solution = get_sum_of_even_numbers_in_fibonacci_sequence(value)
+
+print('The solution for Problem 2 is {}'.format(solution))
