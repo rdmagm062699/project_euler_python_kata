c40272715f025564ef240ec85001194732ced247
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2018-12-25 18:06:10 -0600

    Problem 2 return initial fibonacci

--- src/problem_2/solution/fibonacci.py
+++ src/problem_2/solution/fibonacci.py
@@ -1,3 +1,6 @@
 
 def get_fibonacci_up_to(upper_bound):
+    if upper_bound == 3:
+        return [1, 2]
+
     return []
--- test/test_problem_2/fibonacci_test.py
+++ test/test_problem_2/fibonacci_test.py
@@ -7,3 +7,7 @@ class TestGetFibonacciUpTo:
     def test_returns_empty_list_if_less_than_3(self):
         fibonacci = get_fibonacci_up_to(2)
         assert fibonacci == []
+
+    def test_returns_base_fibonacci_if_3(self):
+        fibonacci = get_fibonacci_up_to(3)
+        assert fibonacci == [1, 2]
