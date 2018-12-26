89ee77c40f22ca534a784aaa4fd199e839bfba7d
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2018-12-25 18:19:35 -0600

    Problem 2 insert actual fibonacci logic

--- src/problem_2/solution/fibonacci.py
+++ src/problem_2/solution/fibonacci.py
@@ -3,4 +3,10 @@ def get_fibonacci_up_to(upper_bound):
     if upper_bound < 3:
         return []
     
-    return [number for number in range(1, upper_bound)]
+    fibonacci = [1, 2]
+    next_num = 3
+    while next_num < upper_bound:
+        fibonacci.append(next_num)
+        next_num = fibonacci[-2] + fibonacci[-1]
+
+    return fibonacci
--- test/test_problem_2/fibonacci_test.py
+++ test/test_problem_2/fibonacci_test.py
@@ -10,8 +10,16 @@ class TestGetFibonacciUpTo:
 
     def test_returns_base_fibonacci_if_3(self):
         fibonacci = get_fibonacci_up_to(3)
-        assert fibonacci == [1, 2]
+        assert fibonacci == [1, 2], 'got {}'.format(fibonacci)
 
     def test_returns_expected_fibonacci_if_4(self):
         fibonacci = get_fibonacci_up_to(4)
-        assert fibonacci == [1, 2, 3]
+        assert fibonacci == [1, 2, 3], 'got {}'.format(fibonacci)
+
+    def test_returns_expected_fibonacci_if_34(self):
+        fibonacci = get_fibonacci_up_to(34)
+        assert fibonacci == [1, 2, 3, 5, 8, 13, 21], 'got {}'.format(fibonacci)
+
+    def test_returns_expected_fibonacci_if_35(self):
+        fibonacci = get_fibonacci_up_to(35)
+        assert fibonacci == [1, 2, 3, 5, 8, 13, 21, 34], 'got {}'.format(fibonacci)
