8b609e3e947f63ae74faebb731e07151698e1d27
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2019-01-02 13:46:48 -0600

    Problem 4 third failing palindrome test

--- test/test_problem_4/palindrome_test.py
+++ test/test_problem_4/palindrome_test.py
@@ -11,3 +11,7 @@ class TestIsPalindrome:
     def test_is_palindrome_11(self):
         test = is_palindrome(11)
         assert test == True, 'got {}'.format(test)
+    
+    def test_is_palindrome_1122(self):
+        test = is_palindrome(1122)
+        assert test == False, 'got {}'.format(test)
