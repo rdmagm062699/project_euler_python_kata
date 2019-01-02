bb26f2f95c92276d2c34fa18fc119e19aa135d4b
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2019-01-02 10:55:00 -0600

    Problem 3 solve primes for 18

--- src/problem_3/solution/primes.py
+++ src/problem_3/solution/primes.py
@@ -3,7 +3,16 @@ def get_primes(number):
     if number <= 3:
         return [number]
 
-    if number == 4:
-        return [2, 2]
+    primes = []
+    next_prime = 2
+    while next_prime < number:
+        if number % next_prime == 0:
+            primes.append(next_prime)
+            number = number / next_prime
+            next_prime = 2
+        else:
+            next_prime += 1
 
-    return [2, 3]
+    primes.append(number)
+
+    return primes
--- test/test_problem_3/primes_test.py
+++ test/test_problem_3/primes_test.py
@@ -7,24 +7,24 @@ class TestGetFibonacciUpTo:
     def test_get_primes_for_2(self):
         primes = get_primes(2)
 
-        assert primes == [2]
+        assert primes == [2], 'got {}'.format(primes)
 
     def test_get_primes_for_3(self):
         primes = get_primes(3)
 
-        assert primes == [3]
+        assert primes == [3], 'got {}'.format(primes)
         
     def test_get_primes_for_4(self):
         primes = get_primes(4)
 
-        assert primes == [2, 2]
+        assert primes == [2, 2], 'got {}'.format(primes)
         
     def test_get_primes_for_6(self):
         primes = get_primes(6)
 
-        assert primes == [2, 3]
+        assert primes == [2, 3], 'got {}'.format(primes)
 
     def test_get_primes_for_28(self):
         primes = get_primes(28)
 
-        assert primes == [2, 2, 7]
+        assert primes == [2, 2, 7], 'got {}'.format(primes)
