4a76131c874c7121a7eca18a87194edd6f7f5e51
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2019-01-10 16:32:41 -0600

    Pass test for palindrom from 2 digit numbers

--- src/problem_4/solution/calculate.py
+++ src/problem_4/solution/calculate.py
@@ -1,3 +1,18 @@
+from .data import get_possible_factors
+from .palindrome import is_palindrome
 
-def get_largest_palindrome(number_of_digits):
-    return 9
+def get_largest_palindrome(num_of_digits):
+    factors = get_possible_factors(num_of_digits)
+    found = False
+
+    for factor_one in sorted(factors, reverse=True):
+        for factor_two in sorted(factors, reverse=True):
+            value = factor_one * factor_two
+            if is_palindrome(value):
+                found = True
+                break
+
+        if found == True:
+            break
+
+    return value
