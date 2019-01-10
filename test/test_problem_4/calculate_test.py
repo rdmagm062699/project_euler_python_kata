47d427c92db97ae234e75b6d91822bf23645b90a
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2019-01-10 16:20:35 -0600

    Failing test for largest palindrome from 2 digit numbers

--- test/test_problem_4/calculate_test.py
+++ test/test_problem_4/calculate_test.py
@@ -7,3 +7,7 @@ class TestGetLargestPalindrome:
     def test_get_largest_palindrome_for_1_digit(self):
         value = get_largest_palindrome(1)
         assert value == 9
+
+    def test_get_largest_palindrome_for_2_digit(self):
+        value = get_largest_palindrome(2)
+        assert value == 9009
