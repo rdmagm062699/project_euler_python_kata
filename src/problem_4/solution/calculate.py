d91294b8e9941497938d95c4d0640f6d6bf31bca
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2019-01-11 08:02:06 -0600

    Avoid duplicate checks in problem 4

--- src/problem_4/solution/calculate.py
+++ src/problem_4/solution/calculate.py
@@ -2,13 +2,16 @@ from .data import get_possible_factors
 from .palindrome import is_palindrome
 
 def get_largest_palindrome(num_of_digits):
-    factors = get_possible_factors(num_of_digits)
+    left_factors = get_possible_factors(num_of_digits)
+    right_factors = left_factors.copy()
     max = 0
 
-    for factor_one in sorted(factors, reverse=True):
-        for factor_two in sorted(factors, reverse=True):
+    for factor_one in left_factors:
+        for factor_two in right_factors:
             value = factor_one * factor_two
             if is_palindrome(value) and value > max:
                 max = value
-                
+
+        right_factors.pop(0)
+
     return max
