d1bddf3430fc470e7c0bfe2636c6c7ec8569d113
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2019-01-14 08:12:14 -0600

    Third failing test for Problem 5 divisor check

--- test/test_problem_5/processor_test.py
+++ test/test_problem_5/processor_test.py
@@ -10,3 +10,7 @@ class TestCheckAllDivisors:
     def test_6_is_evenly_divisible_by_all_humbers_from_1_to_3(self):
         value = check_all_divisors(dividend=6, max_divisor=3)
         assert value == True
+
+    def test_12_is_evenly_divisible_by_all_humbers_from_1_to_4(self):
+        value = check_all_divisors(dividend=12, max_divisor=4)
+        assert value == True
