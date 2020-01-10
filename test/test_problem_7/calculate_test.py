4d1afd758e91827ac1d0e17ba810b225e9c51d8d
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-01-10 14:42:22 -0600

    WIP - Problem 7 failing test for 2nd prime number

--- test/test_problem_7/calculate_test.py
+++ test/test_problem_7/calculate_test.py
@@ -6,3 +6,7 @@ class TestCalculate:
     def test_1st_prime_number(self):
         value = get_nth_prime_number(1)
         assert value == 2, 'got {}'.format(value)
+    
+    def test_2nd_prime_number(self):
+        value = get_nth_prime_number(2)
+        assert value == 3, 'got {}'.format(value)
