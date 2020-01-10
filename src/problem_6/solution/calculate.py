ba3537a551d1fa501d7d4ff20eab0f28af842144
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-01-10 14:00:52 -0600

    WIP - Problem 6 failing test for square of sum function

--- src/problem_6/solution/calculate.py
+++ src/problem_6/solution/calculate.py
@@ -2,3 +2,6 @@
 def get_sum_of_squares(max_number):
     squares = [number ** 2 for number in range(1, max_number + 1)]
     return sum(squares)
+
+def get_square_of_sum(max_number):
+    pass
--- test/test_problem_6/calculate_test.py
+++ test/test_problem_6/calculate_test.py
@@ -1,8 +1,12 @@
 import pytest
 
-from problem_6.solution.calculate import get_sum_of_squares
+from problem_6.solution.calculate import get_sum_of_squares, get_square_of_sum
 
 class TestCalculate:
     def test_get_sum_of_squares_for_max_of_10(self):
         value = get_sum_of_squares(max_number=10)
         assert value == 385, 'got {}'.format(value)
+
+    def test_get_square_of_sum_for_max_of_10(self):
+        value = get_square_of_sum(max_number=10)
+        assert value == 2640 , 'got {}'.format(value)
