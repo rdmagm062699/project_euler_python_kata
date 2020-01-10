c19833ff95c42764edc135e79432e35d41f9a965
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-01-10 15:40:10 -0600

    Prolem 7 allow for retrieving any prime number

--- src/problem_7/solution/calculate.py
+++ src/problem_7/solution/calculate.py
@@ -2,5 +2,19 @@
 def get_nth_prime_number(n):
     if n <= 2:
         return n + 1
-    else:
-        return 5    
+
+    primes = [2, 3]
+    next_num = 4
+    next_multplier = 1
+    next_add = -1
+    while len(primes) < n:
+        if (6 * next_multplier) + next_add == next_num:
+            primes.append(next_num)
+            if next_add > ~0:
+                next_multplier += 1
+            next_add *= -1
+
+        next_num += 1    
+
+    return primes[-1]
+    
