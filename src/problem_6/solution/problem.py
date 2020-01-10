ebd38711910cbe0a6afa0a1da75c30f1d6f953b0
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-01-10 14:10:34 -0600

    Problem 6 function to solve the problem

--- src/problem_6/solution/problem.py
+++ src/problem_6/solution/problem.py
@@ -1,3 +1,4 @@
+from .calculate import get_square_of_sum, get_sum_of_squares
 
 def solve_difference_square_of_sum_and_sum_of_square(max_number):
-    pass
+    return get_square_of_sum(max_number) - get_sum_of_squares(max_number)
