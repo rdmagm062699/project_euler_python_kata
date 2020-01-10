343528c021b4ba533936e30b91779a1becaa5d16
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-01-10 12:46:42 -0600

    Problem 5 add solution script

---
+++ src/problem_5/solve.py
@@ -0,0 +1,5 @@
+from solution.calculate import get_smallest_number_divisible_by_range
+
+solution = get_smallest_number_divisible_by_range(30)
+
+print('The solution for Problem 5 is {}'.format(solution))
