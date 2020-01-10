6e07d296a6c360769bae73a1847aecfefe9ddde0
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-01-10 14:12:35 -0600

    Problem 6 add script to solve

---
+++ src/problem_6/solve.py
@@ -0,0 +1,5 @@
+from solution.problem import solve_difference_square_of_sum_and_sum_of_square
+
+solution = solve_difference_square_of_sum_and_sum_of_square(100)
+
+print('The solution for Problem 6 is {}'.format(solution))
