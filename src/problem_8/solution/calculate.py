18b5a0c2d07aee517adddf869b513ebd7c7b8617
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-01-14 11:34:49 -0600

    Problem 8 refactor logic to a private function

--- src/problem_8/solution/calculate.py
+++ src/problem_8/solution/calculate.py
@@ -3,6 +3,17 @@ def get_largest_product_of_adjacent_digits(number, num_adjacent_digits):
     digits = [int(digit) for digit in str(number)]
     _validate(digits, num_adjacent_digits)
 
+    return _calculate_largest_product(digits, num_adjacent_digits)
+
+def _validate(digits, num_adjacent_digits):
+    if num_adjacent_digits > len(digits):
+        raise Exception(
+            "Number of adjacent digits cannot be larger than the length of the number")
+
+    if num_adjacent_digits < 2:
+        raise Exception("Number of adjacent digits must be greater than 1")
+
+def _calculate_largest_product(digits, num_adjacent_digits):
     start = 0
     max_value = 0
     while start + num_adjacent_digits <= len(digits):
@@ -12,15 +23,6 @@ def get_largest_product_of_adjacent_digits(number, num_adjacent_digits):
         start += 1
 
     return max_value
-
-    
-def _validate(digits, num_adjacent_digits):
-    if num_adjacent_digits > len(digits):
-        raise Exception(
-            "Number of adjacent digits cannot be larger than the length of the number")
-
-    if num_adjacent_digits < 2:
-        raise Exception("Number of adjacent digits must be greater than 1")
 
 def _multiply_list_of_digits(digits):
     result = 1
