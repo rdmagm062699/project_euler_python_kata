283ce594e0eefb6f29ec536f1a6cbb671e51ff93
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-01-14 11:07:03 -0600

    Problem 8 starting over because I read the instructions wrong, it's max product not max sum

--- src/problem_8/solution/calculate.py
+++ src/problem_8/solution/calculate.py
@@ -1,16 +1,6 @@
 
-def get_largest_sum_of_adjacent_digits(number, num_adjacent_digits):
+def get_largest_product_of_adjacent_digits(number, num_adjacent_digits):
     number_string = str(number)
     if num_adjacent_digits > len(number_string):
         raise Exception("Number of adjacent digits cannot be larger than the length of the number")
-    
-    start = 0
-    max_sum = 0
-    while start + num_adjacent_digits <= len(number_string):
-        numbers = [int(number) for number in number_string[start:start+num_adjacent_digits]]
-        value = sum(numbers)
-        if value > max_sum:
-            max_sum = value
-        start += 1
 
-    return max_sum
--- test/test_problem_8/calculate_test.py
+++ test/test_problem_8/calculate_test.py
@@ -1,20 +1,9 @@
 import pytest
 
-from problem_8.solution.calculate import get_largest_sum_of_adjacent_digits
+from problem_8.solution.calculate import get_largest_product_of_adjacent_digits
 
 class TestCalculate:
     def test_number_of_adjacent_digits_must_be_less_than_length_of_number(self):
-       with pytest.raises(Exception):
+       with pytest.raises(Exception) as e:
            get_largest_sum_of_adjacent_digits(number=1, num_adjacent_digits=2)
-    
-    def test_get_largest_sum_of_1_adjacent_digit_within_a_single_digit_number_is_that_number(self):
-        value = get_largest_sum_of_adjacent_digits(5, num_adjacent_digits=1)
-        assert value == 5, "got {}".format(value)
-    
-    def test_get_largest_sum_of_1_adjacent_digit_within_a_multiple_digit_number_is_expected_value(self):
-        value = get_largest_sum_of_adjacent_digits(121, num_adjacent_digits=1)
-        assert value == 2, "got {}".format(value)
-
-    def test_get_largest_sum_of_multiple_adjacent_digits_within_multiple_digit_number_is_expected_value(self):
-        value = get_largest_sum_of_adjacent_digits(12, num_adjacent_digits=2)
-        assert value == 3, "got {}".format(value)
+           assert e == "Number of adjacent digits cannot be larger than the length of the number"
