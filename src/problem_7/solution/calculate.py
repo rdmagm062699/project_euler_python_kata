6b8e80f5906d98aee842fb57d6505a86087776a5
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-01-14 08:06:15 -0600

    Problem 7 - implement the fact that any prime number greater than 3 can be represented by 6n +or- 1 where n is natural number

--- src/problem_7/solution/calculate.py
+++ src/problem_7/solution/calculate.py
@@ -3,4 +3,18 @@ def get_nth_prime_number(n):
     if n <= 2:
         return n + 1
     else:
-        return 5    
+        return _get_n_primes(n)[-1] 
+
+def _get_n_primes(n):
+    primes = [2, 3]
+
+    multiplier = 1
+    add_value = -1
+    while len(primes) < n:
+        next_value = (6 * multiplier) + add_value
+        primes.append(next_value)
+        if add_value > 0:
+            multiplier += 1
+        add_value *= -1
+    
+    return primes
