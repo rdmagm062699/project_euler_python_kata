c469214599807d46ba0a941a32fc617e669ddc92
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-01-14 10:24:39 -0600

    WIP - Problem 8 failing test for 1 adjacent digit in multi digit number

--- test/test_problem_8/calculate_test.py
+++ test/test_problem_8/calculate_test.py
@@ -8,5 +8,7 @@ class TestCalculate:
            get_largest_sum_of_adjacent_digits(number=1, num_adjacent_digits=2)
     
     def test_get_largest_sum_of_1_adjacent_digit_within_a_single_digit_number_is_that_number(self):
-        number = 5
-        assert get_largest_sum_of_adjacent_digits(number, num_adjacent_digits=1) == number
+        assert get_largest_sum_of_adjacent_digits(5, num_adjacent_digits=1) == 5
+    
+    def test_get_largest_sum_of_1_adjacent_digit_within_a_multiple_digit_number_is_that_number(self):
+        assert get_largest_sum_of_adjacent_digits(121, num_adjacent_digits=1) == 2
