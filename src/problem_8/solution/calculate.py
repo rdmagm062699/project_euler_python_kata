e2edafcc85bb05c5909731c80b4742d281aa3fd2
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-01-14 10:22:08 -0600

    Problem 8 handle one adjacent digit for a single digit number

--- src/problem_8/solution/calculate.py
+++ src/problem_8/solution/calculate.py
@@ -1,3 +1,6 @@
 
 def get_largest_sum_of_adjacent_digits(number, num_adjacent_digits):
-    raise Exception("Number of adjacent digits cannot be larger than the length of the number")
+    if num_adjacent_digits > len(str(number)):
+        raise Exception("Number of adjacent digits cannot be larger than the length of the number")
+
+    return number
