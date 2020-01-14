75bc3975cf955d079a19fdff5e717f675b94c2b7
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-01-14 10:19:28 -0600

    WIP - Problem 8 failing test for 1 adjacent digit in a single digit number

--- test/test_problem_8/calculate_test.py
+++ test/test_problem_8/calculate_test.py
@@ -3,6 +3,10 @@ import pytest
 from problem_8.solution.calculate import get_largest_sum_of_adjacent_digits
 
 class TestCalculate:
-   def test_number_of_adjacent_digits_must_be_less_than_length_of_number(self):
+    def test_number_of_adjacent_digits_must_be_less_than_length_of_number(self):
        with pytest.raises(Exception):
            get_largest_sum_of_adjacent_digits(number=1, num_adjacent_digits=2)
+    
+    def test_get_largest_sum_of_1_adjacent_digit_within_a_single_digit_number_is_that_number(self):
+        number = 5
+        assert get_largest_sum_of_adjacent_digits(number, num_adjacent_digits=1) == number
