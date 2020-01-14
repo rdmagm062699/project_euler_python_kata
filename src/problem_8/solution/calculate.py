701bca4cfbc0351afb7ae2859d458c88979c8d8a
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-01-14 11:12:16 -0600

    Problem 8 refactor validation into its own private function

--- src/problem_8/solution/calculate.py
+++ src/problem_8/solution/calculate.py
@@ -1,8 +1,12 @@
 
 def get_largest_product_of_adjacent_digits(number, num_adjacent_digits):
     number_string = str(number)
+    _validate(number_string, num_adjacent_digits)
+    
+def _validate(number_string, num_adjacent_digits):
     if num_adjacent_digits > len(number_string):
-        raise Exception("Number of adjacent digits cannot be larger than the length of the number")
+        raise Exception(
+            "Number of adjacent digits cannot be larger than the length of the number")
 
     if num_adjacent_digits < 2:
         raise Exception("Number of adjacent digits must be greater than 1")
