063175d8f70495f0eb56de55144434e56525cf84
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-01-14 13:17:53 -0600

    WIP - Problem 9 failing test for second primitave pythagorean triple

--- test/test_problem_9/calculate_test.py
+++ test/test_problem_9/calculate_test.py
@@ -6,3 +6,8 @@ class TestPrimitavePythagoreanTriples:
     def test_get_first_primitave_pythagorean_triple(self):
        value = get_nth_primitave_pythagorean_triple(1)
        assert value == (3,4,5), "got {}".format(value)
+
+    def test_get_second_primative_pythagorean_triple(self):
+       value = get_nth_primitave_pythagorean_triple(2)
+       assert value == (5, 12, 13), "got {}".format(value)
+
