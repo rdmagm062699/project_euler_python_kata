7f29e85575662bce4733453912af7f9df5e79962
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-01-14 11:18:32 -0600

    WIP - Problem 8 failing test for 2 adjacent digits in a 2 digit number

--- test/test_problem_8/calculate_test.py
+++ test/test_problem_8/calculate_test.py
@@ -12,3 +12,7 @@ class TestCalculate:
        with pytest.raises(Exception) as e:
            get_largest_sum_of_adjacent_digits(number=11, num_adjacent_digits=1)
            assert e == "Number of adjacent digits must be greater than 1"
+
+    def test_max_product_of_2_adjacent_digits_in_a_2_digits_number_is_expected_value(self):
+        value = get_largest_product_of_adjacent_digits(44, 2)
+        assert value == 16, "got {}".format(value)
