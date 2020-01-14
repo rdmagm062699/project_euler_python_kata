5a1e43b78a87db79db7a26130e0b2d2172b9aaa8
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-01-14 11:26:22 -0600

    Problem 8 handle 3 adjacent digits in a 3 digit number

--- src/problem_8/solution/calculate.py
+++ src/problem_8/solution/calculate.py
@@ -3,7 +3,7 @@ def get_largest_product_of_adjacent_digits(number, num_adjacent_digits):
     digits = [int(digit) for digit in str(number)]
     _validate(digits, num_adjacent_digits)
 
-    return digits[0] * digits[1]
+    return _multiply_list_of_digits(digits)
 
     
 def _validate(digits, num_adjacent_digits):
@@ -13,4 +13,11 @@ def _validate(digits, num_adjacent_digits):
 
     if num_adjacent_digits < 2:
         raise Exception("Number of adjacent digits must be greater than 1")
+
+def _multiply_list_of_digits(digits):
+    result = 1
+    for digit in digits:
+        result = result * digit
+
+    return result
 
