35112221e034ac7aefe603395fae588a0ae2032f
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-01-22 15:34:48 -0600

    Problem 9 add solution script

---
+++ src/problem_9/solve.py
@@ -0,0 +1,5 @@
+from solution.problem import get_product_of_triple_where_sum_is_n
+
+solution = get_product_of_triple_where_sum_is_n(1000)
+
+print('The solution for Problem 9 is {}'.format(solution))
