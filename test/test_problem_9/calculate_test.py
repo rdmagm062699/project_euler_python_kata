41d9abbed1f336cc34bf07d2ac6404d189d08cd9
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-01-22 14:53:55 -0600

    WIP - Problem 9 failing test for finding triple where sum is 12

--- test/test_problem_9/calculate_test.py
+++ test/test_problem_9/calculate_test.py
@@ -1,1 +1,8 @@
 import pytest
+
+from src.problem_9.solution.calculate import get_triple_where_sum_is_n
+
+class TestGetTripleWhereSumIsN:
+    def test_sum_12(self):
+        value = get_triple_where_sum_is_n(12)
+        assert value == (3, 4, 5)
