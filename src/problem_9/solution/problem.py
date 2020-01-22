ead41c5c4e0fad070d921fc1b62b32137047c777
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-01-22 15:17:02 -0600

    Problem 9 passing test for product of triple where sum is 30

--- src/problem_9/solution/problem.py
+++ src/problem_9/solution/problem.py
@@ -1,3 +1,5 @@
+from .calculate import get_pythagorean_triple_product, get_triple_where_sum_is_n
 
 def get_product_of_triple_where_sum_is_n(n):
-    pass
+    triple = get_triple_where_sum_is_n(n)
+    return get_pythagorean_triple_product(triple)
