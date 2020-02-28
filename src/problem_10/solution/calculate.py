33c0cb13ed3b35685967026a6cc76beaaf0f9e47
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-02-28 13:18:56 -0600

    Problem 10 use Sieve of Eratosthenes algorithm to get list of primes

--- src/problem_10/solution/calculate.py
+++ src/problem_10/solution/calculate.py
@@ -3,5 +3,17 @@ def get_sum_of_primes_less_than_n(n):
     if n <= 2:
         return 0
     
-    primes = [2,3]
+    primes = _get_primes_less_than_n(n)
     return sum([x for x in primes if x < n])
+
+
+def _get_primes_less_than_n(n):
+    check = [True for i in range(n+1)]
+    possiblePrime = 2
+    while (possiblePrime**2 <= n):
+        if (check[possiblePrime] == True):
+            for i in range(possiblePrime**2, n+1, possiblePrime):
+                check[i] = False
+        possiblePrime += 1
+
+    return [idx for (idx,value) in enumerate(check) if idx >= 2 and value == True]
