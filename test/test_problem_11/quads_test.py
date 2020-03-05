09c5acbf836aa02f2b632f7ef1bd181f9783f0c5
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-03-05 13:39:19 -0600

    WIP - Problem 11 failing test expecting four adjacent digits to be multiplied

--- test/test_problem_11/quads_test.py
+++ test/test_problem_11/quads_test.py
@@ -7,3 +7,8 @@ class TestGetMaxProductOfAdjacent:
         list = [1,1,1,1]
         value = get_max_product_of_adjacent(list)
         assert value == 1
+
+    def test_list_of_four_different_numbers_returns_product_of_those_four(self):
+        list = [1, 2, 3, 4]
+        value = get_max_product_of_adjacent(list)
+        assert value == 24
