991e34f671022619dcd54ff722e2ee2b1da5990a
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-03-05 13:45:06 -0600

    WIP - Problem 11 - failing test expecting traversal of list to get max product of four adjacent numbers

--- test/test_problem_11/quads_test.py
+++ test/test_problem_11/quads_test.py
@@ -12,3 +12,8 @@ class TestGetMaxProductOfAdjacent:
         list = [1, 2, 3, 4]
         value = get_max_product_of_adjacent(list)
         assert value == 24
+
+    def test_list_of_five_different_numbers_returns_max_product_of_four_adjacent(self):
+        list = [2, 2, 3, 4, 5]
+        value = get_max_product_of_adjacent(list)
+        assert value == 120
