547eb97b28013c173d24d084a8143362fab993b0
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-03-06 08:35:48 -0600

    WIP - Problem 11 - failing test expecting the opposite of each diagonal to be evaluated

--- test/test_problem_11/diagonals_test.py
+++ test/test_problem_11/diagonals_test.py
@@ -10,3 +10,11 @@ class TestGetMaxProductFromAllDiagonals:
                 [0, 0, 0, 4]]
         value = get_max_product_from_all_diagonals(grid)
         assert value == 24
+
+    def test_single_diagonal_top_left_to_bottom_right_and_its_opposite(self):
+        grid = [[1, 0, 0, 5],
+                [0, 2, 4, 0],
+                [0, 3, 3, 0],
+                [2, 0, 0, 4]]
+        value = get_max_product_from_all_diagonals(grid)
+        assert value == 120
