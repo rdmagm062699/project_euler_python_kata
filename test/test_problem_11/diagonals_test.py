42f7c7c5ce197e8c6da794bf4d2d0f1500899d51
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-03-06 13:21:22 -0600

    WIP - Problem 11 - failing test looking for something other than the center diagonals

--- test/test_problem_11/diagonals_test.py
+++ test/test_problem_11/diagonals_test.py
@@ -18,3 +18,12 @@ class TestGetMaxProductFromAllDiagonals:
                 [2, 0, 0, 4]]
         value = get_max_product_from_all_diagonals(grid)
         assert value == 120
+
+    def test_diagonal_that_is_not_the_center_diagonal(self):
+        grid = [[1, 2, 0, 0, 0],
+                [0, 2, 3, 0, 0],
+                [0, 0, 3, 4, 0],
+                [0, 0, 0, 4, 5],
+                [0, 0, 0, 0, 0]]
+        value = get_max_product_from_all_diagonals(grid)
+        assert value == 120, 'got {}'.format(value)
