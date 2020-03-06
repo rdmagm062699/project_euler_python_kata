2f8eeac26c1c88174007a8cd0f359274d64e7597
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-03-06 08:32:20 -0600

    Problem 11 - get max adjacnt product for single top left to bottom right diagonal

--- src/problem_11/solution/diagonals.py
+++ src/problem_11/solution/diagonals.py
@@ -1,4 +1,5 @@
 from src.problem_11.solution.quads import get_max_product_of_adjacent
 
 def get_max_product_from_all_diagonals(grid):
-    pass
+    diagonal = [grid[x][x] for x in range(0, len(grid))]
+    return get_max_product_of_adjacent(diagonal)
