4b30a992b06a46e15f61298d18888284a5c8589e
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-06-19 12:31:12 -0500

    Problem 11 - Calculates when a column has the max product

--- src/problem_11/solution/calculate.py
+++ src/problem_11/solution/calculate.py
@@ -1,4 +1,10 @@
 from src.problem_11.solution.rows import get_max_product_from_all_rows
+from src.problem_11.solution.colulmns import get_max_product_from_all_columns
 
 def get_max_product_from_adjacent_quads(grid):
-    return get_max_product_from_all_rows(grid)
+    values = [
+        get_max_product_from_all_rows(grid),
+        get_max_product_from_all_columns(grid)
+    ]
+
+    return max(values)
