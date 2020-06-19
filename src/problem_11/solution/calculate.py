0afb1bf71ceb840092160e81ed240bcd50f55a65
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-06-19 12:33:01 -0500

    Problem 11 - Calculates when a diagonal has the max product

--- src/problem_11/solution/calculate.py
+++ src/problem_11/solution/calculate.py
@@ -1,10 +1,12 @@
 from src.problem_11.solution.rows import get_max_product_from_all_rows
 from src.problem_11.solution.colulmns import get_max_product_from_all_columns
+from src.problem_11.solution.diagonals import get_max_product_from_all_diagonals
 
 def get_max_product_from_adjacent_quads(grid):
     values = [
         get_max_product_from_all_rows(grid),
-        get_max_product_from_all_columns(grid)
+        get_max_product_from_all_columns(grid),
+        get_max_product_from_all_diagonals(grid)
     ]
 
     return max(values)
