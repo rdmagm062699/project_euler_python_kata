18e0ee6014d15a3536b7580576aceac786c61328
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-06-19 14:26:12 -0500

    WIP - failing test for number of divisors for 3

--- test/test_problem_12/divisors_test.py
+++ test/test_problem_12/divisors_test.py
@@ -7,3 +7,7 @@ class TestGetNumberOfDivisors:
     def test_get_number_of_divisors_for_1(self):
         divisors = get_number_of_divisors(1)
         assert divisors == 1, 'got {}'.format(divisors)
+
+    def test_get_number_of_divisors_for_3(self):
+        divisors = get_number_of_divisors(3)
+        assert divisors == 2, 'got {}'.format(divisors)
