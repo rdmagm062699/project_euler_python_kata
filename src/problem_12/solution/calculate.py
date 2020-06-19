520c06874073c947680990566bed56066d18fa46
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-06-19 15:00:37 -0500

    Problem 12 - first triangular with more than 5 divisors

--- src/problem_12/solution/calculate.py
+++ src/problem_12/solution/calculate.py
@@ -1,6 +1,13 @@
+from .divisors import get_number_of_divisors
 
 def find_triangular_number_divisible_by_more_than_n_numbers(n):
-    if n > 0:
-        return 3
-        
-    return 1
+    count = 1
+    triangular = 0
+    divisors = 0
+
+    while divisors <= n:
+        triangular = triangular + count
+        divisors = get_number_of_divisors(triangular)
+        count += 1
+
+    return triangular
