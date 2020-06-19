ef2ef1369c1b74e3911ca63859cf093528e6047c
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-06-19 14:49:53 -0500

    WIP - Problem 12 - failing test for first triangular divisible by more than one number

--- test/test_problem_12/calculate_test.py
+++ test/test_problem_12/calculate_test.py
@@ -7,3 +7,7 @@ class TestHighlyDivisibleTriangular:
     def test_first_triangular_divisible_by_more_than_zero_numbers(self):
         triangular = find_triangular_number_divisible_by_more_than_n_numbers(0)
         assert triangular == 1, 'got {}'.format(triangular)
+
+    def test_first_triangular_divisible_by_more_than_one_numbers(self):
+        triangular = find_triangular_number_divisible_by_more_than_n_numbers(1)
+        assert triangular == 3, 'got {}'.format(triangular)
