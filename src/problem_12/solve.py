ed6a57b007ebeb628785a8d3ad269397aae643b0
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-06-19 15:18:17 -0500

    Problem 12 - add solution script

---
+++ src/problem_12/solve.py
@@ -0,0 +1,5 @@
+from solution.calculate import find_triangular_number_divisible_by_more_than_n_numbers
+
+solution = find_triangular_number_divisible_by_more_than_n_numbers(500)
+
+print('The solution for Problem 12 is {}'.format(solution))
