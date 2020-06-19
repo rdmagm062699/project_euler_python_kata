a4a3dc301e192589b8ab9c17b7043214439866f2
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-06-19 14:55:26 -0500

    WIP - Problem 12 - Failing test for first triangular with more than 5 divisors

--- test/test_problem_12/calculate_test.py
+++ test/test_problem_12/calculate_test.py
@@ -11,3 +11,7 @@ class TestHighlyDivisibleTriangular:
     def test_first_triangular_divisible_by_more_than_one_numbers(self):
         triangular = find_triangular_number_divisible_by_more_than_n_numbers(1)
         assert triangular == 3, 'got {}'.format(triangular)
+
+    def test_first_triangular_divisible_by_more_than_five_numbers(self):
+        triangular = find_triangular_number_divisible_by_more_than_n_numbers(5)
+        assert triangular == 28, 'got {}'.format(triangular)
