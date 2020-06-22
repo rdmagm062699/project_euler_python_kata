9c2c3dd487f7dd82d00a7c9795a4d013952c2d3d
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-06-22 10:50:04 -0500

    WIP - Problem 14 - failing test for longest collatz with max starting number 2

--- test/test_problem_14/calculate_test.py
+++ test/test_problem_14/calculate_test.py
@@ -7,3 +7,8 @@ class TestGetLongestCollatzSequence:
     def test_longest_sequence_with_max_starting_number_1(self):
         value = get_longest_collatz_sequence(1)
         assert value == 1, 'got {}'.format(value)
+        
+    
+    def test_longest_sequence_with_max_starting_number_2(self):
+        value = get_longest_collatz_sequence(2)
+        assert value == 1, 'got {}'.format(value)
