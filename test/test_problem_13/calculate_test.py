335674bfb4a15c0e63d3af2e36de1110dc2246d5
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-06-22 09:15:29 -0500

    WIP - Problem 13 - failing test to force sum and get first n digits

--- test/test_problem_13/calculate_test.py
+++ test/test_problem_13/calculate_test.py
@@ -10,6 +10,12 @@ class TestGetDigitsOfSum:
         assert value == '0', 'got {}'.format(value)
 
     def test_sum_of_one_and_nine_first_1_digit_returns_1(self):
-        numbers = [1,9]
+        numbers = [1, 9]
         value = get_first_n_digits_of_sum(numbers, 1)
         assert value == '1', 'got {}'.format(value)
+
+    def test_sum_of_eleven_and_twelve_first_2_digits_returns_23(self):
+        numbers = [11, 12]
+        value = get_first_n_digits_of_sum(numbers, 1)
+        assert value == '23', 'got {}'.format(value)
+
