396fbb798eddfe9bab517100143218d222248990
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-06-22 14:29:50 -0500

    Problem 17 - pass test for count of letters in number names from 1 to 1

--- src/problem_17/solution/calculate.py
+++ src/problem_17/solution/calculate.py
@@ -2,4 +2,6 @@ from .numbers import get_number_name, count_letters_in_name
 
 
 def get_number_of_letters_in_number_names_from_1_to_n(n):
-    pass
+    number_name = get_number_name(n)
+
+    return count_letters_in_name(number_name)
