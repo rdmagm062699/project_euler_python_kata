d7205fea8ddea0fe9f86e5bd9ced7bfee557e7b6
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-06-22 10:34:00 -0500

    WIP - Problem 14 - failing test to get sequence starting at 1

--- test/test_problem_14/collatz_test.py
+++ test/test_problem_14/collatz_test.py
@@ -17,4 +17,10 @@ class TestGetCollatzSequence:
     def test_sequence_starting_at_3(self):
         starting_number = 3
         sequence = get_sequence(starting_number)
-        assert sequence == [3, 10, 5, 16, 8, 4, 2, 1], 'got {}'.format(sequence)
+        assert sequence == [3, 10, 5, 16, 8,
+                            4, 2, 1], 'got {}'.format(sequence)
+
+    def test_sequence_starting_at_1(self):
+        starting_number = 1
+        sequence = get_sequence(starting_number)
+        assert sequence == [1, 4, 2, 1], 'got {}'.format(sequence)
