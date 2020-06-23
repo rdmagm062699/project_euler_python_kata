a81d8a97e0a0b3593655647f26b2fcc30f506df3
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-06-23 12:24:46 -0500

    Problem 18 - solve for simple three row triangle

--- src/problem_18/solution/calculate.py
+++ src/problem_18/solution/calculate.py
@@ -1,9 +1,12 @@
 
 def get_max_path_down_triangle_of_numbers(triangle):
     max_sum = triangle[0][0]
+    last_max_pos = 0
 
     for row_num in range(1, len(triangle)):
         row = triangle[row_num]
-        max_sum += max(row)
+        max_value = max([row[last_max_pos], row[last_max_pos + 1]])
+        last_max_pos = row.index(max_value)
+        max_sum += max_value
 
     return max_sum
