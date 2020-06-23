1f70d9ff25560e1fe5d318707bb7fd436549d02b
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-06-23 12:06:36 -0500

    Problem 18 - solve for a simple two row triangle

--- src/problem_18/solution/calculate.py
+++ src/problem_18/solution/calculate.py
@@ -1,3 +1,9 @@
 
 def get_max_path_down_triangle_of_numbers(triangle):
-    return triangle[0][0]
+    max_sum = triangle[0][0]
+
+    for row_num in range(1, len(triangle)):
+        row = triangle[row_num]
+        max_sum += max(row)
+
+    return max_sum
