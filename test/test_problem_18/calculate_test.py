728b7c07df7a46dcdf249647ad1c8f789e2f6fb3
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-06-23 12:00:51 -0500

    WIP - Problem 18 - failing test for simple two row triangle

--- test/test_problem_18/calculate_test.py
+++ test/test_problem_18/calculate_test.py
@@ -4,8 +4,15 @@ from problem_18.solution.calculate import get_max_path_down_triangle_of_numbers
 
 class TestMaxPathDownTriangleOfNumbers(unittest.TestCase):
 
-    def test_single_row_with_2_is_2(self):
+    def test_single_row_triangle(self):
         triangle = [
             [2]
         ]
         self.assertEqual(get_max_path_down_triangle_of_numbers(triangle), 2);
+    
+    def test_two_row_triangle(self):
+        triangle = [
+              [1],
+            [2, 3]
+        ]
+        self.assertEqual(get_max_path_down_triangle_of_numbers(triangle), 4)
