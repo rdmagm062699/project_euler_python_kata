baa99752ae6bedb84d71e01110c3d057ba46ce26
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-06-24 14:47:41 -0500

    Problem 20 - function to get sum of digits in a factorial

--- src/problem_20/solution/calculate.py
+++ src/problem_20/solution/calculate.py
@@ -1,4 +1,6 @@
-
+from .number import get_factorial, get_sum_of_digits
 
 def get_sum_of_digits_of_a_factorial(number):
-    pass
+    factorial = get_factorial(number)
+
+    return get_sum_of_digits(factorial)
