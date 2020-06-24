92a940daab06e993b74461c477b5eeab8e6b824b
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-06-24 13:53:48 -0500

    WIP - Problem 19 - failing test for number of first month Sundays in 2001

--- test/test_problem_19/calculate_test.py
+++ test/test_problem_19/calculate_test.py
@@ -6,3 +6,6 @@ class TestFirstMonthSundays(unittest.TestCase):
     
     def test_2000_has_3_sundays_on_the_first_of_the_month(self):
         self.assertEqual(count_first_of_month_sundays(2000), 3)
+
+    def test_2001_has_2_sundays_on_the_first_of_the_month(self):
+        self.assertEqual(count_first_of_month_sundays(2001), 2)
