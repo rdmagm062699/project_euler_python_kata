7086803b87014dba66e60cc3c9c886aa81a43af3
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-06-24 14:41:41 -0500

    Problem 20 - finish factorial function

--- src/problem_20/solution/number.py
+++ src/problem_20/solution/number.py
@@ -6,4 +6,9 @@ def get_sum_of_digits(number):
     return sum(digits)
 
 def get_factorial(number):
-    return number
+    result = 1
+
+    for n in range(1, number + 1):
+        result *= n 
+
+    return result
