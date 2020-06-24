411774215253275f98ed47ace95dfda15c009c0e
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-06-24 12:21:13 -0500

    Problem 18 - refactor some things into local functions

--- src/problem_18/solution/calculate.py
+++ src/problem_18/solution/calculate.py
@@ -10,15 +10,23 @@ class NumberTriangle:
             prev_positions = max_pos_tracker[row_num - 1]
             current_row = self.rows[row_num]
 
-            positions_to_check = set()
-            for pos in prev_positions:
-                positions_to_check.add(pos)
-                positions_to_check.add(pos + 1)
-
-            values_to_check = [value for (idx, value) in enumerate(current_row) if idx in positions_to_check]
-            max_value = max(values_to_check)
-            max_positions = [pos for pos in positions_to_check if current_row[pos] == max_value]
+            positions_to_check = self._get_app_positions_to_check_in_current_row(prev_positions)
+            max_positions = self._get_positions_of_max_values_in_current_row(current_row, positions_to_check)
             max_pos_tracker.append(max_positions)
 
         max_values = [self.rows[idx][pos[0]] for (idx,pos) in enumerate(max_pos_tracker)]
         return sum(max_values)
+
+    def _get_app_positions_to_check_in_current_row(self, prev_positions):
+        positions = set()
+        for pos in prev_positions:
+            positions.add(pos)
+            positions.add(pos + 1)
+
+        return positions
+
+    def _get_positions_of_max_values_in_current_row(self, row, positions_to_check):
+        values_to_check = [value for (idx, value) in enumerate(row) if idx in positions_to_check]
+        max_value = max(values_to_check)
+
+        return [pos for pos in positions_to_check if row[pos] == max_value]
