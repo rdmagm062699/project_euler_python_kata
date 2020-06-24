671f90625d2c0c8498ebd275f0359314686fcc5e
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-06-24 14:40:08 -0500

    WIP - Problem 20 - failing test for factorial of 3

--- test/test_problem_20/number_test.py
+++ test/test_problem_20/number_test.py
@@ -14,3 +14,6 @@ class TestFactorial(unittest.TestCase):
 
     def test_factorial_of_2_is_2(self):
         self.assertEqual(get_factorial(2), 2)
+
+    def test_factorial_of_3_is_6(self):
+        self.assertEqual(get_factorial(3), 6)
