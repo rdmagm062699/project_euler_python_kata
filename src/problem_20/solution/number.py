91d0209dc70a34ed772e2e727f5cb56f04efdd1b
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-06-24 14:38:35 -0500

    WIP - Problem 20 - failing test for factorial of 2

---
+++ src/problem_20/solution/number.py
@@ -0,0 +1,9 @@
+
+
+def get_sum_of_digits(number):
+    digits = map(int, str(number))
+
+    return sum(digits)
+
+def get_factorial(number):
+    pass
---
+++ test/test_problem_20/number_test.py
@@ -0,0 +1,16 @@
+import unittest
+
+from problem_20.solution.number import get_sum_of_digits, get_factorial
+
+class TestSumOfDigits(unittest.TestCase):
+
+    def test_one_returns_1(self):
+        self.assertEqual(get_sum_of_digits(1), 1)
+
+    def test_eleven_returns_2(self):
+        self.assertEqual(get_sum_of_digits(11), 2)
+
+class TestFactorial(unittest.TestCase):
+
+    def test_factorial_of_2_is_2(self):
+        self.assertEqual(get_factorial(2), 2)
