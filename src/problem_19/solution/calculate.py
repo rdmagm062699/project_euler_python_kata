1e473846ba9401f40dc2cc97e52700a24cfcd2bc
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-06-24 14:03:23 -0500

    Problem 19 - calculate first of month Sundays for range of years

--- src/problem_19/solution/calculate.py
+++ src/problem_19/solution/calculate.py
@@ -1,3 +1,9 @@
+from .year_calcs import count_first_of_month_sundays
 
 def count_first_of_month_sundays_for_range_of_years(first_year, last_year):
-    pass
+    count = 0
+
+    for year in range(first_year, last_year + 1):
+        count += count_first_of_month_sundays(year)
+
+    return count
