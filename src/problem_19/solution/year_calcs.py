bd4471a9de4ac089b1b07940516b654434a9bf42
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-06-24 13:58:28 -0500

    Problem 19 - fix the tests and calculation for Sundays on the first of the month in a year

--- src/problem_19/solution/year_calcs.py
+++ src/problem_19/solution/year_calcs.py
@@ -1,3 +1,10 @@
+import datetime
 
 def count_first_of_month_sundays(year):
-    return 3
+    count = 0
+
+    for month in range(1, 13):
+        checkdate = datetime.datetime(year, month, 1)
+        count += 1 if checkdate.weekday() == 6 else 0
+
+    return count
--- test/test_problem_19/calculate_test.py
+++ test/test_problem_19/calculate_test.py
@@ -4,8 +4,8 @@ from problem_19.solution.year_calcs import count_first_of_month_sundays
 
 class TestFirstMonthSundays(unittest.TestCase):
     
-    def test_2000_has_3_sundays_on_the_first_of_the_month(self):
-        self.assertEqual(count_first_of_month_sundays(2000), 3)
+    def test_2000_has_1_sunday_on_the_first_of_the_month(self):
+        self.assertEqual(count_first_of_month_sundays(2000), 1)
 
     def test_2001_has_2_sundays_on_the_first_of_the_month(self):
         self.assertEqual(count_first_of_month_sundays(2001), 2)
