527bdd62cabea47c79307be659479c44c2f1bb13
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-06-24 14:05:32 -0500

    Problem 19 - add solution script

---
+++ src/problem_19/solve.py
@@ -0,0 +1,5 @@
+from solution.calculate import count_first_of_month_sundays_for_range_of_years
+
+solution = count_first_of_month_sundays_for_range_of_years(1901, 2000)
+
+print('The solution for Problem 19 is {}'.format(solution))
