c2556487d788ee42ab23555bd51f8ecf1df9745f
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-06-26 09:20:25 -0500

    WIP - Problem 21 - failing test for proper divisors of 2

---
+++ src/problem_21/solution/number.py
@@ -0,0 +1,3 @@
+
+def get_proper_divisors(number):
+    pass
---
+++ test/test_problem_21/number_test.py
@@ -0,0 +1,8 @@
+import unittest
+
+from problem_21.solution.number import get_proper_divisors
+
+class TestGetProperDivisors(unittest.TestCase):
+
+    def test_only_proper_divisor_of_2_is_1(self):
+        self.assertEqual(get_proper_divisors(2), 1)
