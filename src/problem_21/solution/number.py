cbeffd5f5ebb348b6151ba076e26bd379f413d39
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-06-26 09:36:17 -0500

    Problem 21 - solve for proper divisors of 4

--- src/problem_21/solution/number.py
+++ src/problem_21/solution/number.py
@@ -4,5 +4,8 @@ def get_proper_divisors(number):
 
     if number > 1:
         result.append(1)
+        for div in range(2, number):
+            if number % div == 0:
+                result.append(div)
 
     return result
