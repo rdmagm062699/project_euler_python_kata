bc75cdf05386b31e9c3670f67b39aa157b0e8b0a
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-06-26 09:27:40 -0500

    Problem 21 - expect an array of proper divisors

--- src/problem_21/solution/number.py
+++ src/problem_21/solution/number.py
@@ -1,3 +1,3 @@
 
 def get_proper_divisors(number):
-    return 1
+    return [1]
--- test/test_problem_21/number_test.py
+++ test/test_problem_21/number_test.py
@@ -5,4 +5,4 @@ from problem_21.solution.number import get_proper_divisors
 class TestGetProperDivisors(unittest.TestCase):
 
     def test_only_proper_divisor_of_2_is_1(self):
-        self.assertEqual(get_proper_divisors(2), 1)
+        self.assertEqual(get_proper_divisors(2), [1])
