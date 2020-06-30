72b679517ad09cfea8c1347b8f6e9638ec54d7c5
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-06-30 15:03:18 -0500

    Problem 21 - add solution script

---
+++ src/problem_21/solve.py
@@ -0,0 +1,5 @@
+from solution.number import get_sum_of_amicable_numbers_less_than_n
+
+solution = get_sum_of_amicable_numbers_less_than_n(10000)
+
+print('The solution for Problem 21 is {}'.format(solution))
