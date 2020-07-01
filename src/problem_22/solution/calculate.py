8a6e9cbee335267c4250644f2641c176ca4c1fdf
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-07-01 10:52:55 -0500

    Problem 22 - refactor for loop

--- src/problem_22/solution/calculate.py
+++ src/problem_22/solution/calculate.py
@@ -3,9 +3,7 @@ from .name import score_name
 def score_list_of_names(names): 
     score = 0
 
-    pos = 1
-    for name in names:
-        score += score_name(name, pos)
-        pos += 1
+    for (pos, name) in enumerate(names):
+        score += score_name(name, pos + 1)
 
     return score
