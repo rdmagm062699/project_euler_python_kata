da603ec53b6d93b77ef90d471116307e5669fa36
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-07-06 15:38:41 -0500

    WIP - Problem 28 - failing test for a 5 by 5 spiral

--- test/test_problem_28/spiral_test.py
+++ test/test_problem_28/spiral_test.py
@@ -12,3 +12,14 @@ class TestBuildSpiral(unittest.TestCase):
         ] 
         self.assertEqual(build_spiral(3), expected)
 
+
+    def test_spiral_in_a_5_by_5_grid(self):
+        expected = [
+            [21, 22, 23, 24, 25],
+            [20, 7, 8, 9, 10], 
+            [19, 6, 1, 2, 11],
+            [18, 5, 4, 3, 12],
+            [17, 16, 15, 14, 13]
+        ]
+
+        self.assertEqual(build_spiral(5), expected)
