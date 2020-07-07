5528ad1a6bb9883e63298fe5af658b19ef52a423
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2020-07-07 08:46:09 -0500

    Problem 28 - refactors sprial class into class that holds its own state while building the spiral

--- src/problem_28/solution/spiral.py
+++ src/problem_28/solution/spiral.py
@@ -2,62 +2,68 @@
 class Spiral:
     def __init__(self, grid_size):
         self.grid = self._build_initial_grid(grid_size)
+
+        middle = int(len(self.grid) / 2)
+
+        self._right_boundary = middle + 1
+        self._bottom_boundary = middle + 1
+        self._left_boundary = middle - 1
+        self._top_boundary = middle - 1
+        self._next_move = "right"
+        self._move_to_pos = (middle, middle)
         self._build_spiral()
 
     def _build_spiral(self):
-        middle = int(len(self.grid) / 2)
-
-        right_boundary = middle + 1
-        bottom_boundary = middle + 1
-        left_boundary = middle - 1
-        top_boundary = middle - 1
-        next_move = "right"
         next_number = 1
-        current_pos = (middle, middle)
 
-        while current_pos != (0, len(self.grid)):
-            row = current_pos[0]
-            col = current_pos[1]
+        while self._move_to_pos != (0, len(self.grid)):
+            row = self._move_to_pos[0]
+            col = self._move_to_pos[1]
 
             self.grid[row][col] = next_number
 
             next_number += 1
 
-            if next_move == "right":
-                current_pos = (row, col + 1)
+            move_direction = self._next_move
+            self._calculate_next_move(move_direction, row, col)
 
-                if current_pos[1] == right_boundary:
-                    right_boundary += 1
-                    next_move = "down"
+
+    def _calculate_next_move(self, move_direction, row, col):
+            if move_direction == "right":
+                self._move_to_pos = (row, col + 1)
+
+                if self._move_to_pos[1] == self._right_boundary:
+                    self._right_boundary += 1
+                    self._next_move = "down"
                 else:
-                    next_move = "right"
+                    self._next_move = "right"
 
-            elif next_move == "down":
-                current_pos = (row + 1, col)
+            elif move_direction == "down":
+                self._move_to_pos = (row + 1, col)
 
-                if current_pos[0] == bottom_boundary:
-                    bottom_boundary += 1
-                    next_move = "left"
+                if self._move_to_pos[0] == self._bottom_boundary:
+                    self._bottom_boundary += 1
+                    self._next_move = "left"
                 else:
-                    next_move = "down"
+                    self._next_move = "down"
 
-            elif next_move == "left":
-                current_pos = (row, col - 1)
+            elif move_direction == "left":
+                self._move_to_pos = (row, col - 1)
 
-                if current_pos[1] == left_boundary:
-                    left_boundary -= 1
-                    next_move = "up"
+                if self._move_to_pos[1] == self._left_boundary:
+                    self._left_boundary -= 1
+                    self._next_move = "up"
                 else:
-                    next_move = "left"
+                    self._next_move = "left"
 
-            elif next_move == "up":
-                current_pos = (row - 1, col)
+            elif move_direction == "up":
+                self._move_to_pos = (row - 1, col)
 
-                if current_pos[0] == top_boundary:
-                    top_boundary -= 1
-                    next_move = "right"
+                if self._move_to_pos[0] == self._top_boundary:
+                    self._top_boundary -= 1
+                    self._next_move = "right"
                 else:
-                    next_move = "up"
+                    self._next_move = "up"
 
 
     def _build_initial_grid(self, grid_size):
