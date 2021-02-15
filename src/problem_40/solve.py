95ac5a594adf1cb6ac2f5360db7ab56868bc32eb
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2021-02-15 16:08:34 -0600

    Problem 40 - add solution script

--- src/problem_40/solve.py
+++ src/problem_40/solve.py
@@ -1,6 +1,12 @@
-# from solution.spiral import Spiral
+from solution.irrational_decimal import get_fraction_digits
 
-# spiral = Spiral(1001)
-# solution = spiral.get_sum_of_diagonals()
+digit_one = int(get_fraction_digits(1)[-1])
+digit_two = int(get_fraction_digits(10)[-1])
+digit_three = int(get_fraction_digits(100)[-1])
+digit_four = int(get_fraction_digits(1000)[-1])
+digit_five = int(get_fraction_digits(10000)[-1])
+digit_six = int(get_fraction_digits(100000)[-1])
+digit_seven = int(get_fraction_digits(1000000)[-1])
 
-# print('The solution for Problem 28 is {}'.format(solution))
+solution = digit_one * digit_two * digit_three * digit_four * digit_five * digit_six * digit_seven
+print('The solution for Problem 40 is {}'.format(solution))
