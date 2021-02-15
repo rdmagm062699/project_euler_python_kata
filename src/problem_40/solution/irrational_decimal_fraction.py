f9cc9d982622be25550e4f2ecd7c87f998551b95
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2021-02-15 15:37:25 -0600

    Problem 40 - refactor

--- src/problem_40/solution/irrational_decimal_fraction.py
+++ src/problem_40/solution/irrational_decimal_fraction.py
@@ -1,9 +1,6 @@
 
 class IrrationalDecimalFraction:
     def __init__(self, length):
-        self.value = ''
-
         integers = '123456789'
 
-        if length > 0:
-            self.value = integers[0:length]
+        self.value = integers[0:length]
