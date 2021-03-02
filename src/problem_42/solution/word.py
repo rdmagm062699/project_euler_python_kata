a00aed87c025a9dfca6b1c2846b318e491275475
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2021-03-02 16:19:41 -0600

    Problem 42 - word value of AAZ is twenty-eight

--- src/problem_42/solution/word.py
+++ src/problem_42/solution/word.py
@@ -1,3 +1,10 @@
 
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
 def get_word_value(word):
-    return len(word)
+    value = 0
+    for letter in word:
+        letter_num = LETTERS.index(letter) + 1
+        value += letter_num
+
+    return value
--- test/test_problem_42/word_test.py
+++ test/test_problem_42/word_test.py
@@ -10,3 +10,6 @@ class TestWordValue(unittest.TestCase):
 
     def test_value_of_word_AA(self):
         self.assertEqual(get_word_value('AA'), 2)
+    
+    def test_value_of_word_AAZ(self):
+        self.assertEqual(get_word_value('AAZ'), 28)
