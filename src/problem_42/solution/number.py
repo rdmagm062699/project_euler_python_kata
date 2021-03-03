8d7c5cb0a1ae2175c9873e4bfe94aa19661f98d4
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2021-03-03 08:08:37 -0600

    Problem 42 - simplify triangle number check

--- src/problem_42/solution/number.py
+++ src/problem_42/solution/number.py
@@ -1,16 +1,8 @@
+import math
 
 def is_triangle_number(number):
-    n = 1
-    stop = False
+    if number <= 0:
+        return False
 
-    while stop == False:
-        t_num = get_nth_triangle_number(n)
-        if t_num >= number:
-            stop = True
-        else:
-            n += 1
-
-    return number == t_num
-
-def get_nth_triangle_number(n):
-    return (.5 * n) * (n + 1)
+    check_val = (number * 8) + 1
+    return math.sqrt(check_val).is_integer()
