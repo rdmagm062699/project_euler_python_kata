4318aadd7789de1c96b532cf01b0bb4d96be6e68
rdmagm062699 <rob.miller@homeinsteadinc.com>
2021-06-17 08:30:42 -0500

    Problem 48 - power sequence for 1 to 2 is 5

--- src/problem_48/solution/powers.py
+++ src/problem_48/solution/powers.py
@@ -1,3 +1,6 @@
 
 def get_power_sequence(start, end):
+    if end > 1:
+        return 5
+        
     return 1
--- test/test_problem_48/powers_test.py
+++ test/test_problem_48/powers_test.py
@@ -7,3 +7,6 @@ class TestPowers(unittest.TestCase):
 
     def test_range_of_1_returns_1(self):
         self.assertEqual(get_power_sequence(1, 1), 1)
+
+    def test_range_of_1_to_2_returns_5(self):
+        self.assertEqual(get_power_sequence(1, 2), 5)
