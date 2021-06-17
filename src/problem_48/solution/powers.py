7a9a6db1a8d8c9d4d96d3e247fc2153f937e9f2d
rdmagm062699 <rob.miller@homeinsteadinc.com>
2021-06-17 08:27:10 -0500

    Problem 48 - initial setup

---
+++ src/problem_48/solution/powers.py
@@ -0,0 +1,3 @@
+
+def get_power_sequence():
+    pass
---
+++ test/test_problem_48/powers_test.py
@@ -0,0 +1,9 @@
+import unittest
+
+from problem_48.solution.powers import get_power_sequence
+
+
+class TestPowers(unittest.TestCase):
+
+    def test_blah(self):
+        self.assertEqual(1, 1)
