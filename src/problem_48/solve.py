59326e7e41de65112c227f971697e1159d3dc8bb
rdmagm062699 <rob.miller@homeinsteadinc.com>
2021-06-17 08:41:06 -0500

    Problem 48 - add solution script

---
+++ src/problem_48/solve.py
@@ -0,0 +1,6 @@
+from solution.powers import get_power_sequence_sum
+
+value = get_power_sequence_sum(1, 1000)
+solution = str(value)[-10:]
+
+print('The solution for Problem 48 is {}'.format(solution))
